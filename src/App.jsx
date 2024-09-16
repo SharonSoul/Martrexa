31658dfdccd2df609a25f1f580ee52c1a7f14cb7
Sharon Olaitan <105372243+SharonSoul@users.noreply.github.com>
2024-09-17 00:12:15 +0100

    Files added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { useState } from 'react'
 import './App.css'
+import HeroNav from './components/HeroNav'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <h1 className="text-3xl font-bold underline text-red-500">
-      Hello world!
-    </h1>
+    <HeroNav />
   )
 }
 
---
+++ src/components/HeroNav.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './../styles/hero.css'
+
+const HeroNav = () => {
+  return (
+    <div>heroNav</div>
+  )
+}
+
+export default HeroNav;
